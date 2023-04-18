5fe560d9463ae0fc81c7a32e1e6aef49fad49fa7
cszsm <zscseh93@gmail.com>
2023-04-18 20:52:24 +0200

    remove separator

--- lib/widgets/identifier/attribute_category_selector/attribute_category_selector.dart
+++ lib/widgets/identifier/attribute_category_selector/attribute_category_selector.dart
@@ -16,6 +16,7 @@ class AttributeCategorySelector extends StatefulWidget {
 }
 
 class _AttributeCategorySelectorState extends State<AttributeCategorySelector> {
+  final GlobalKey<AnimatedListState> _listKey = GlobalKey();
   final Map<String, String> selection = {};
   final List<String> defaultCategoryIds = getAttributeCategoryIds();
   List<String> unselectedCategoryIds = getAttributeCategoryIds();
@@ -34,6 +35,7 @@ class _AttributeCategorySelectorState extends State<AttributeCategorySelector> {
       } else {
         selection[categoryId] = newSelectedValueId;
         unselectedCategoryIds.remove(categoryId);
+        // _listKey.currentState!.insertItem(selection.length, duration: const Duration(seconds: 1));
       }
     });
 
@@ -53,10 +55,7 @@ class _AttributeCategorySelectorState extends State<AttributeCategorySelector> {
     int itemCount = selection.length + unselectedCategoryIds.length;
     itemCount = selection.isNotEmpty ? itemCount + 1 : itemCount;
 
-    return ListView.separated(
-      separatorBuilder: (BuildContext context, int index) {
-        return const SizedBox(height: 8);
-      },
+    return ListView.builder(
       itemCount: itemCount,
       itemBuilder: (BuildContext context, int index) {
         if (index < selection.length) {
@@ -65,7 +64,9 @@ class _AttributeCategorySelectorState extends State<AttributeCategorySelector> {
 
           return Padding(
             padding: EdgeInsets.only(
-              top: index == 0 ? 400 : 0,
+              top: index == 0 ? 400 : 8,
+              left: 10,
+              right: 10,
             ),
             child: SelectedCategoryTile(
               categoryId: categoryId,
@@ -74,19 +75,22 @@ class _AttributeCategorySelectorState extends State<AttributeCategorySelector> {
             ),
           );
         } else if (selection.isNotEmpty && index == selection.length) {
-          return SizedBox(
-            height: 50,
-            child: Padding(
-              padding: const EdgeInsets.only(bottom: 0),
-              child: Align(
-                alignment: Alignment.centerRight,
-                child: Padding(
-                  padding: const EdgeInsets.only(right: 22),
-                  child: selection.isNotEmpty
-                      ? ClearFilterButton(
-                          onTap: resetSelection,
-                        )
-                      : Container(),
+          return Padding(
+            padding: const EdgeInsets.only(top: 8),
+            child: SizedBox(
+              height: 50,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 0),
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 22),
+                    child: selection.isNotEmpty
+                        ? ClearFilterButton(
+                            onTap: resetSelection,
+                          )
+                        : Container(),
+                  ),
                 ),
               ),
             ),
@@ -99,8 +103,10 @@ class _AttributeCategorySelectorState extends State<AttributeCategorySelector> {
 
           return Padding(
             padding: EdgeInsets.only(
-              top: index == 0 ? 400 : 0,
+              top: index == 0 ? 400 : 8,
               bottom: index == itemCount - 1 ? 90 : 0,
+              left: 10,
+              right: 10,
             ),
             child: UnselectedCategoryTile(
               categoryId: categoryId,
