9a9020025d417eee1378e37031d5abf7719fd443
cszsm <zscseh93@gmail.com>
2023-05-30 22:17:42 +0200

    Loading with progress indicator

--- lib/services/image_loader.dart
+++ lib/services/image_loader.dart
@@ -24,51 +24,66 @@ class ImageLoader {
     plantsDirectory = Directory(plantsDirectoryPath);
   }
 
-  Future<void> _downloadZip(bool test) async {
+  Stream<double> _downloadZip(bool test) async* {
     final refUrl =
         test ? '$baseUrl/$pathTest/$filename' : '$baseUrl/$pathProd/$filename';
     final ref = FirebaseStorage.instance.refFromURL(refUrl);
-
-    final completer = Completer();
-
     final downloadTask = ref.writeToFile(zipFile);
-    downloadTask.snapshotEvents.listen((event) {
+
+    await for (final event in downloadTask.snapshotEvents) {
       switch (event.state) {
         case TaskState.running:
+          yield event.bytesTransferred / event.totalBytes;
+          break;
         case TaskState.paused:
-          print('totalBytes');
-          print(event.totalBytes);
-          print('bytesTransferred');
-          print(event.bytesTransferred);
           break;
         case TaskState.success:
         case TaskState.canceled:
         case TaskState.error:
-          completer.complete();
-          break;
+          return;
       }
-    });
-
-    return completer.future;
-  }
-
-  Future<void> _unzip() async {
-    try {
-      await ZipFile.extractToDirectory(
-          zipFile: zipFile, destinationDir: plantsDirectory);
-    } catch (e) {
-      print(e);
     }
   }
 
-  Future<void> download(bool test) async {
-    await _downloadZip(test);
-    await _unzip();
+  Stream<double> _unzip() {
+    late StreamController<double> controller;
+
+    void startUnzip() {
+      try {
+        ZipFile.extractToDirectory(
+          zipFile: zipFile,
+          destinationDir: plantsDirectory,
+          onExtracting: (zipEntry, progress) {
+            controller.add(progress / 100);
+            return ZipFileOperation.includeItem;
+          },
+        ).then((value) => controller.close());
+      } catch (e) {
+        // TODO: handle error properly
+        print(e);
+      }
+    }
+
+    controller = StreamController<double>(
+      onListen: startUnzip,
+    );
+    return controller.stream;
+  }
+
+  Stream<double> download(bool test) async* {
+    await for (final downloadProgress in _downloadZip(test)) {
+      yield downloadProgress / 2;
+    }
+
+    await for (final unzipProgress in _unzip()) {
+      yield unzipProgress / 2 + 0.5;
+    }
+
     await zipFile.delete();
 
-    await applicationDirectory.list().forEach((element) {
-      print(element);
-    });
+    // await applicationDirectory.list().forEach((element) {
+    //   print(element);
+    // });
   }
 
   isDownloadNeeded() async {
